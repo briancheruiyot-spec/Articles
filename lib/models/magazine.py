3d30c5481480ba82fe4457492aeef437b7a3bd23
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-25 22:20:16 +0300

    added save method

--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -8,4 +8,13 @@ class Magazine:
     self.name = name
     self.category = category
 
-  
+  def save(self):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+      "INSERT INTO magazines (name, category) VALUES (?, ?)",
+      (self.name, self.category)
+    )
+    self.id = cursor.lastrowid
+    conn.commit()
+    conn.close() 
